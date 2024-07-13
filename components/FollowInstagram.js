73b01d4e235ebc81da307e4bb9ced0d6648941a8
Lakshya Kashyap <yashjha0241@gmail.com>
2024-07-13 12:28:23 +0530

    fixed instagram marquee

--- components/FollowInstagram.js
+++ components/FollowInstagram.js
@@ -11,30 +11,27 @@ const FollowInstagram = () => {
             </div>
 
             <div className='flex lg:hidden'>
-                <Marquee pauseOnHover speed={100} play>
-                    <div className="instagramModels flex items-center mt-10 w-[calc(100vw-40px)] overflow-x-auto xl:overflow-x-hidden">
-
-                        <img src="assets/models/3.png" alt="Model" />
-                        <img src="assets/models/4.png" alt="Model" />
-                        <img src="assets/models/5.png" alt="Model" />
-                        <img src="assets/models/6.png" alt="Model" />
-                        <img src="assets/models/7.png" alt="Model" />
-                        <img src="assets/models/8.png" alt="Model" />
-                        <img src="assets/models/9.png" alt="Model" />
-                    </div>
+                <Marquee pauseOnHover speed={50}>
+                    <img src="assets/models/3.png" alt="Model" />
+                    <img src="assets/models/4.png" alt="Model" />
+                    <img src="assets/models/5.png" alt="Model" />
+                    <img src="assets/models/6.png" alt="Model" />
+                    <img src="assets/models/7.png" alt="Model" />
+                    <img src="assets/models/8.png" alt="Model" />
+                    <img src="assets/models/9.png" alt="Model" />
                 </Marquee>
             </div>
             <div className='lg:flex hidden'>
-                    <div className="instagramModels flex items-center mt-10 w-[calc(100vw-40px)] overflow-x-auto xl:overflow-x-hidden">
+                <div className="instagramModels flex items-center mt-10 w-[calc(100vw-40px)] overflow-x-auto xl:overflow-x-hidden">
 
-                        <img src="assets/models/3.png" alt="Model" />
-                        <img src="assets/models/4.png" alt="Model" />
-                        <img src="assets/models/5.png" alt="Model" />
-                        <img src="assets/models/6.png" alt="Model" />
-                        <img src="assets/models/7.png" alt="Model" />
-                        <img src="assets/models/8.png" alt="Model" />
-                        <img src="assets/models/9.png" alt="Model" />
-                    </div>
+                    <img src="assets/models/3.png" alt="Model" />
+                    <img src="assets/models/4.png" alt="Model" />
+                    <img src="assets/models/5.png" alt="Model" />
+                    <img src="assets/models/6.png" alt="Model" />
+                    <img src="assets/models/7.png" alt="Model" />
+                    <img src="assets/models/8.png" alt="Model" />
+                    <img src="assets/models/9.png" alt="Model" />
+                </div>
             </div>
 
 
